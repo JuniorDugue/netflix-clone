8c69f3b0cbd14f4d97fd0d601ef7508f003868bd
juniordugue <jrdugue@gmail.com>
2020-11-05 23:57:24 -0500

    refactored app component, added new style components in the index along with styles for the jumbotron component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import jumboData from "./fixtures/jumbo.json";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1>Netflix Clone</h1>
       <Jumbotron.Container>
         {jumboData.map((item) => (
           <Jumbotron key={item.id} direction={item.direction}>
-            <p>{item.title}</p>
-            <p>{item.subTitle}</p>
-            <p>{item.image}</p>
-            <p>{item.alt}</p>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>
-    </div>
+    </>
   );
 }
 
