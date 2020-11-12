15f6dd09389e5a1e94d8ca42b356be77ea57c998
juniordugue <jrdugue@gmail.com>
2020-11-12 14:11:32 -0500

    Added globalstyle file & imported in it inside of index.js, including css reset via normalize.css, created pages and container folders

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Inner, Pane, Title, SubTitle, Image } from "./styles/jumbotron";
+import { Container, Inner, Item, Pane, Title, SubTitle, Image } from "./styles/jumbotron";
 
 export default function Jumbotron({ children, direction = "row", ...restProps }) {
   return (
     <div>
-      <Inner direction={direction}>
-        <p>{children}</p>
-      </Inner>
+      <Item {...restProps}>
+        <Inner direction={direction}>{children}</Inner>
+      </Item>
     </div>
   );
 }
